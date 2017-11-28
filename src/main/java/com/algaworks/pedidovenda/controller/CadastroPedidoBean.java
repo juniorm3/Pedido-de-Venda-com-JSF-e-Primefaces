ee993197a6d494ad532ead6cdf1f34453ad03174
Mario Junior <juniorm3@msn.com>
2017-11-28 16:53:22 -0200

    Tela para cadastro de pedidos

---
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroPedidoBean.java
@@ -0,0 +1,28 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidoBean {
+
+	private List<Integer> itens;
+	
+	public CadastroPedidoBean() {
+		itens = new ArrayList<>();
+		itens.add(1);
+	}
+	
+	/*public void salvar() {
+		throw new NegocioException("Pedido não pode ser salvo, pois ainda não foi implementado!");
+	}*/
+
+	
+	public List<Integer> getItens() {
+		return itens;
+	}
+}
